ea82d66033bd4cb3e3704d84cd2bdd38bfa87a51
NarsiMybooks <narsi.mybooks@gmail.com>
2023-12-22 20:40:23 +0530

    gralvm demo app
    gralvm demo app

---
+++ gralvm_demo/src/main/java/com/weshopifyplatform/app/GralvmDemoApplication.java
@@ -0,0 +1,14 @@
+package com.weshopifyplatform.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GralvmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GralvmDemoApplication.class, args);		
+	}
+
+	
+}
